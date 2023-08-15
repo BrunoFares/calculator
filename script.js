29beb8d962255e70cd60dff8d5ee47c41856acbb
BrunoFares <faresbruno04@gmail.com>
2023-08-15 04:29:54 +0300

    Changed the font of the text and added a '0' to the screen div of the calculator. Added an id to the display of the calculator, and modified the numpad so that the numbers from 0 to 9 all have a common class instead of separate ids, so that it is easier to work with in the .js file". Added most of the query selectors, and created a screenDisplays() function that stores the contents of the screen as it is being added to it. Also added the event listeners for the numbers and the signs, made sure that 2 signs cannot be placed after each other. Created an operate() function which will be the core of operations, and linked it to the event listener of the equals sign."

--- script.js
+++ script.js
@@ -1,11 +1,57 @@
+const firstNumber = 0;
+const operator = '';
+const secondNumber = 0;
+
+const display = document.querySelector('div#screen');
+const number = document.querySelectorAll('div.number');
+const plusSign = document.querySelector('div#plus-sign');
+const minusSign = document.querySelector('div#minus-sign');
+const timesSign = document.querySelector('div#times-sign');
+const divisionSign = document.querySelector('div#division-sign');
+const equals = document.querySelector('div#equals');
+
+for (let i = 0; i < number.length; i++) {
+    number[i].addEventListener('click', () => {
+        screenDisplays(number[i].textContent)
+    });
+}
+
+plusSign.addEventListener('click', () => {
+    if (displayValue[displayValue.length-1] !== '+'
+     && displayValue[displayValue.length-1] !== '-'
+     && displayValue[displayValue.length-1] !== '*'     
+     && displayValue[displayValue.length-1] !== '/') screenDisplays('+');
+})
+
+minusSign.addEventListener('click', () => {
+    if (displayValue[displayValue.length-1] !== '+'
+     && displayValue[displayValue.length-1] !== '-'
+     && displayValue[displayValue.length-1] !== '*'     
+     && displayValue[displayValue.length-1] !== '/') screenDisplays('-');
+})
+
+timesSign.addEventListener('click', () => {
+    if (displayValue[displayValue.length-1] !== '+'
+     && displayValue[displayValue.length-1] !== '-'
+     && displayValue[displayValue.length-1] !== '*'     
+     && displayValue[displayValue.length-1] !== '/') screenDisplays('*');
+})
+
+divisionSign.addEventListener('click', () => {
+    if (displayValue[displayValue.length-1] !== '+'
+     && displayValue[displayValue.length-1] !== '-'
+     && displayValue[displayValue.length-1] !== '*'     
+     && displayValue[displayValue.length-1] !== '/') screenDisplays('/');
+})
+
+equals.addEventListener('click', operate);
+
 function add(...array) {
-    return array.reduce((sum, item) => sum + item, 0);
+    return newArray.reduce((sum, item) => sum + item, 0);
 }
 
 function subtract(...array) {
-    return array.reduce((diff, item) => {
-        diff - item
-    }, array[0]*2);
+    return array.reduce((diff, item) => diff - item);
 }
 
 function multiply(...array) {
@@ -13,7 +59,16 @@ function multiply(...array) {
 }
 
 function divide(...array) {
-    return array.reduce((total, item) => {
-        total / item
-    }, array[0]**2);
+    return array.reduce((total, item) => total / item);
+}
+
+function operate() {
+    if (+displayValue[displayValue.length - 1] === 'number') {
+        console.log(false);
+    }   
+}
+
+let displayValue;
+function screenDisplays(item) {
+    displayValue = display.textContent += item;
 }
