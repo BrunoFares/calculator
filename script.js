ac0ffcf1b0a05b511ab7fa2fbf4aea9cec831f02
BrunoFares <faresbruno04@gmail.com>
2023-08-15 19:22:36 +0300

    Worked on the operate() function by dividing the operation into two arrays: one for the numbers and another for the operands. Followed it with a switch statement that enters each two individual numbers and returns them according to the sign between them. Also refined the screenDisplays function with now two states, open and closed. When closed, the function call erases what was previously written on the display, and when open, it adds to it. Simplified the add, subtract, multiply, and divide functions as I realised they were unnecessarily complicated because the design to the rest of the code was a lot simpler.

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const firstNumber = 0;
-const operator = '';
-const secondNumber = 0;
+let displayValue = 0;
 
 const display = document.querySelector('div#screen');
 const number = document.querySelectorAll('div.number');
@@ -9,66 +7,111 @@ const minusSign = document.querySelector('div#minus-sign');
 const timesSign = document.querySelector('div#times-sign');
 const divisionSign = document.querySelector('div#division-sign');
 const equals = document.querySelector('div#equals');
+const reset = document.querySelector('div#reset');
 
 for (let i = 0; i < number.length; i++) {
     number[i].addEventListener('click', () => {
-        screenDisplays(number[i].textContent)
+        if (displayValue == 0) screenDisplays(number[i].textContent, 'closed')
+        else if (displayValue != 0) screenDisplays(number[i].textContent, 'open')
     });
 }
 
+reset.addEventListener('click', () => screenDisplays(0, 'closed'));
+
 plusSign.addEventListener('click', () => {
-    if (displayValue[displayValue.length-1] !== '+'
-     && displayValue[displayValue.length-1] !== '-'
-     && displayValue[displayValue.length-1] !== '*'     
-     && displayValue[displayValue.length-1] !== '/') screenDisplays('+');
+    if (displayValue === undefined) screenDisplays('+', 'open')
+    else if (displayValue[displayValue.length-1] !== '+'
+          && displayValue[displayValue.length-1] !== '-'
+          && displayValue[displayValue.length-1] !== '*'     
+          && displayValue[displayValue.length-1] !== '/') { 
+            screenDisplays('+', 'open');
+        }
+    
 })
 
 minusSign.addEventListener('click', () => {
-    if (displayValue[displayValue.length-1] !== '+'
-     && displayValue[displayValue.length-1] !== '-'
-     && displayValue[displayValue.length-1] !== '*'     
-     && displayValue[displayValue.length-1] !== '/') screenDisplays('-');
+    if (displayValue === undefined) screenDisplays('-', 'open')
+    else if (displayValue[displayValue.length-1] !== '+'
+          && displayValue[displayValue.length-1] !== '-'
+          && displayValue[displayValue.length-1] !== '*'     
+          && displayValue[displayValue.length-1] !== '/') { 
+            screenDisplays('-', 'open');
+        }
 })
 
 timesSign.addEventListener('click', () => {
-    if (displayValue[displayValue.length-1] !== '+'
-     && displayValue[displayValue.length-1] !== '-'
-     && displayValue[displayValue.length-1] !== '*'     
-     && displayValue[displayValue.length-1] !== '/') screenDisplays('*');
+    if (displayValue === undefined) screenDisplays('*', 'open')
+    else if (displayValue[displayValue.length-1] !== '+'
+          && displayValue[displayValue.length-1] !== '-'
+          && displayValue[displayValue.length-1] !== '*'     
+          && displayValue[displayValue.length-1] !== '/') { 
+            screenDisplays('*', 'open');
+        }
 })
 
 divisionSign.addEventListener('click', () => {
-    if (displayValue[displayValue.length-1] !== '+'
-     && displayValue[displayValue.length-1] !== '-'
-     && displayValue[displayValue.length-1] !== '*'     
-     && displayValue[displayValue.length-1] !== '/') screenDisplays('/');
+    if (displayValue === undefined) screenDisplays('/', 'open')
+    else if (displayValue[displayValue.length-1] !== '+'
+          && displayValue[displayValue.length-1] !== '-'
+          && displayValue[displayValue.length-1] !== '*'     
+          && displayValue[displayValue.length-1] !== '/') { 
+            screenDisplays('/', 'open');
+        }
 })
 
 equals.addEventListener('click', operate);
 
-function add(...array) {
-    return newArray.reduce((sum, item) => sum + item, 0);
+function add(a, b) {
+    return a + b;
 }
 
-function subtract(...array) {
-    return array.reduce((diff, item) => diff - item);
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiply(...array) {
-    return array.reduce((prod, item) => prod * item, 1);
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide(...array) {
-    return array.reduce((total, item) => total / item);
+function divide(a, b) {
+    return a / b;
 }
 
 function operate() {
-    if (+displayValue[displayValue.length - 1] === 'number') {
-        console.log(false);
-    }   
+    let opArray = displayValue.split('')
+
+    let opNums = displayValue.split(/[-*+/]+/)
+    let opOpers = opArray.filter(item => {
+        return item === '+'
+            || item === '-'
+            || item === '*'
+            || item === '/';
+    })
+
+    for (let i = 0; i < opNums.length; i++) {
+        switch (opOpers[i]){
+        case '+':
+            opNums[i] = add(+opNums[i], +opNums[i+1])
+            break;
+        case '-':
+            opNums[i] = subtract(+opNums[i], +opNums[i+1])
+            break;
+        case '*':
+            opNums[i] = multiply(+opNums[i], +opNums[i+1])
+            break;
+        case '/':
+            opNums[i] = divide(+opNums[i], +opNums[i+1])
+            break;
+        }
+    }
+    screenDisplays(opNums[0], 'closed');
 }
 
-let displayValue;
-function screenDisplays(item) {
-    displayValue = display.textContent += item;
+
+function screenDisplays(item, setting) {
+    if (setting === 'closed') {
+        displayValue = display.textContent = item;
+    } else if (setting === 'open') {
+        displayValue = display.textContent += item;
+    }
 }
